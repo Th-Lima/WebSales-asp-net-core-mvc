4ffe85bc98ff2a4546574669f1c7653eb05d02f9
Thales Lima <lthales53@gmail.com>
2020-03-23 18:45:35 -0300

    Seller service and basic FindAll

--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSalesMVC.Data;
+using WebSalesMVC.Services;
 
 namespace WebSalesMVC
 {
@@ -36,6 +37,7 @@ namespace WebSalesMVC
                   builder.MigrationsAssembly("WebSalesMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
