838df26536f06f3d52c6fd13bb18742e83bf8405
Thales Lima <lthales53@gmail.com>
2020-03-23 19:45:33 -0300

    Simple create form

--- WebSalesMVC/Controllers/SellersController.cs
+++ WebSalesMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesMVC.Models;
 using WebSalesMVC.Services;
 
 namespace WebSalesMVC.Controllers
@@ -14,6 +15,18 @@ namespace WebSalesMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
