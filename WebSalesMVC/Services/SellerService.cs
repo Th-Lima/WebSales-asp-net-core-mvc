889a4c72f9f78107259ef62d102b769baa8fc348
Thales Lima <lthales53@gmail.com>
2020-03-26 19:01:11 -0300

    Seller details and eager loading

--- WebSalesMVC/Services/SellerService.cs
+++ WebSalesMVC/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using WebSalesMVC.Data;
 using WebSalesMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSalesMVC.Services
 {
@@ -24,7 +25,7 @@ namespace WebSalesMVC.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
